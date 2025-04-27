366ec849b9eda27d6e900873ee5f593d30b4dddf
Mehedi Hasan Emon <ononnoemon14@gmail.com>
2025-04-27 07:33:51 +0600

    prifile context updated

--- src/context/ProfileContext.jsx
+++ src/context/ProfileContext.jsx
@@ -4,7 +4,7 @@ export const ProfileData=createContext()
 
 export default function ProfileContext({children}) {
     
-    const userData={name:'Emsu',age:30}
+    const userData={name:'Emsu',country:'Bangladesh',age:30}
   return (
     <ProfileData.Provider value={userData}>
         {children}
